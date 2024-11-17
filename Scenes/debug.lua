e27c9cdfc36b9086dac3ee50dacff90bd08222d1
KINGTUT10101 <kingtut10101@gmail.com>
2024-11-17 12:32:31 -0600

    Add crosshairs to debug scene

--- Scenes/debug.lua
+++ Scenes/debug.lua
@@ -8,6 +8,8 @@ local biggerFont = love.graphics.newFont (32)
 local showFPS = true
 local showCursorPos = true
 local showRenderStats = false
+local showScreenCrosshair = false
+local showMouseCrosshair = false
 
 function thisScene:load (...)
     if DevMode == false then
@@ -20,7 +22,9 @@ function thisScene:update (dt)
     
 end
 
-function thisScene:draw ()
+function thisScene:lateDraw ()
+    local mx, my = push:toGame(love.mouse.getPosition ())
+
     love.graphics.setFont (biggerFont)
 
     -- FPS
@@ -33,7 +37,6 @@ function thisScene:draw ()
 
     -- Show mouse postion
     if showCursorPos == true then
-        local mx, my = push:toGame(love.mouse.getPosition ())
         love.graphics.setColor ({0, 0, 0, 0.35})
         love.graphics.rectangle ("fill", 1720, 0, 200, 50)
         love.graphics.setColor ({1, 1, 1, 1})
@@ -60,6 +63,22 @@ function thisScene:draw ()
         love.graphics.setColor ({1, 1, 1, 1})
         love.graphics.printf ("Tux Debug", 1720, 1040, 200, "center") -- Mouse position
     end
+
+    -- Screen crosshair
+    if showScreenCrosshair == true then
+        love.graphics.setColor(0, 0, 1, 1)
+        love.graphics.line (GAMEWIDTH / 2, 0, GAMEWIDTH / 2, 1080)
+        love.graphics.line (0, GAMEHEIGHT / 2, 1920, GAMEHEIGHT / 2)
+    end
+
+    -- Mouse crosshair
+    if showMouseCrosshair == true then
+
+
+        love.graphics.setColor(1, 0, 0, 1)
+        love.graphics.line (mx, 0, mx, 1080)
+        love.graphics.line (0, my, 1920, my)
+    end
 end
 
 function thisScene:keypressed (key, scancode, isrepeat)
@@ -71,6 +90,10 @@ function thisScene:keypressed (key, scancode, isrepeat)
         showRenderStats = not showRenderStats
     elseif key == "kp3" then
         tux.utils.setDebugMode (not tux.utils.getDebugMode ())
+    elseif key == "kp4" then
+        showScreenCrosshair = not showScreenCrosshair
+    elseif key == "kp5" then
+        showMouseCrosshair = not showMouseCrosshair
     end
 end
 
