a3ea36fe1d319c1d263bfc1e072b481066406af7
KINGTUT10101 <kingtut10101@gmail.com>
2024-11-18 13:18:33 -0600

    Add component template

---
+++ UI/componentTemplate.lua
@@ -0,0 +1,9 @@
+local tux = require ("Libraries.tux")
+local layout = require ("Helpers.layout")
+local icons = require ("Helpers.icons")
+
+local function nutStats (x, y, nutObj)
+    
+end
+
+return nutStats
