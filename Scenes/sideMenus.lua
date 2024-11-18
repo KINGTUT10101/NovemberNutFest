0a94902c1a3d5e2119154f0cff2c308bb2fff933
KINGTUT10101 <kingtut10101@gmail.com>
2024-11-18 13:23:49 -0600

    Add nut stats menu

---
+++ Scenes/sideMenus.lua
@@ -0,0 +1,38 @@
+local thisScene = {}
+local sceneMan = require ("Libraries.sceneMan")
+local tux = require ("Libraries.tux")
+local Nut = require ("Core.Nut")
+local layout = require ("Helpers.layout")
+local icons = require ("Helpers.icons")
+
+local nutStats = require ("UI.nutStats")
+
+local nutStatsMode = "combat"
+local testNut = Nut:new ()
+
+function thisScene:load (...)
+    
+    
+end
+
+function thisScene:update (dt)
+    nutStatsMode = nutStats (15, 575, testNut, nutStatsMode)
+end
+
+function thisScene:draw ()
+    
+end
+
+function thisScene:lateDraw ()
+    
+end
+
+function thisScene:keypressed (key, scancode, isrepeat)
+	
+end
+
+function thisScene:mousereleased (x, y, button)
+
+end
+
+return thisScene
