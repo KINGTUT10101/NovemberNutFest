b538005b6806769152c5cbdf121a427334a9d33c
KINGTUT10101 <45105509+KINGTUT10101@users.noreply.github.com>
2024-11-28 13:20:36 -0600

    Add new inventory manager

---
+++ Core/newInventoryHandler.lua
@@ -0,0 +1,50 @@
+local clamp = require ("Libraries.lume").clamp
+
+local inventoryHandler = {
+    hotbar = {}, -- Holds the nuts in the player's inventory
+    size = 0, -- Number of nuts in the inventory
+    maxSlots = 10, -- Max number of hotbar slots
+    maxAmmmo  = 500, -- Max amount of ammo
+    currAmmo = 0, -- Current amount of ammo
+}
+
+function inventoryHandler:replaceNut (nutObj, hotbarIndex)
+    assert (type (nutObj) == "table", "Provided nut object is an invalid data type")
+    assert (hotbarIndex >= 1 and hotbarIndex <= self.maxSlots, "Hotbar index out of bounds")
+
+    if self.hotbar[hotbarIndex] == nil then
+        self.size = self.size + 1
+    end
+
+    self.hotbar[hotbarIndex] = nutObj
+end
+
+function inventoryHandler:removeNut (hotbarIndex)
+    assert (hotbarIndex >= 1 and hotbarIndex <= self.maxSlots, "Hotbar index out of bounds")
+
+    if self.hotbar[hotbarIndex] ~= nil then
+        self.size = self.size - 1
+    end
+
+    self.hotbar[hotbarIndex] = nil
+end
+
+function inventoryHandler:getNut (hotbarIndex)
+    assert (hotbarIndex >= 1 and hotbarIndex <= self.maxSlots, "Hotbar index out of bounds")
+
+    return self.hotbar[hotbarIndex]
+end
+
+function inventoryHandler:getAmmoCount ()
+    return self.currAmmo
+end
+
+function inventoryHandler:setAmmoCount (amount)
+    self.currAmmo = clamp (amount, 0, self.maxAmmmo)
+end
+
+function inventoryHandler:addAmmoCount (amount)
+    self.currAmmo = clamp (self.currAmmo + amount, 0, self.maxAmmmo)
+end
+
+return inventoryHandler
