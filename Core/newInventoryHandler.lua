80b4b377817acced12a2e30a4bae3291380052e2
KINGTUT10101 <45105509+KINGTUT10101@users.noreply.github.com>
2024-11-28 21:19:27 -0600

    Add getSize method to inv handler

--- Core/newInventoryHandler.lua
+++ Core/newInventoryHandler.lua
@@ -9,6 +9,10 @@ local inventoryHandler = {
     activeSlot = 1, -- Currently selected hotbar slot
 }
 
+function inventoryHandler:getSize ()
+    return self.size
+end
+
 function inventoryHandler:getMaxSlots ()
     return self.maxSlots
 end
